8db25e4e11ff08bb4f9c497293c5929c46b643e8
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2017-03-03 11:35:11 +0100

    :package: completed services root component

--- static/js/app.js
+++ static/js/app.js
@@ -15,17 +15,6 @@ const
 Vue.use(VueRouter);
 //=============================================================================
 /**
- * components
- */
-//=============================================================================
-const
-    AppNav = require('./components/app_nav'),
-    AppHome = require('./components/app_home'),
-    AppAbout = require('./components/app_about'),
-    AppServices = require('./components/app_services'),
-    AppFooter = require('./components/app_footer');
-//=============================================================================
-/**
  * router
  */
 //=============================================================================
@@ -34,20 +23,43 @@ const
         {
             path: '/',
             name: 'home',
-            component: AppHome
+            component: require('./components/app_home')
         },
         {
             path: '/about',
             name: 'about',
-            component: AppAbout
+            component: require('./components/app_about')
         },
         {
             path: '/services',
             name: 'services',
-            component: AppServices
+            component: require('./components/app_services')
+        },
+        {
+            path: '/services/cloud',
+            name: 'cloud',
+            component: require('./components/app_services_cloud')
+        },
+        {
+            path: '/services/infosec',
+            name: 'infosec',
+            component: require('./components/app_services_infosec')
+        },
+        {
+            path: '/services/portfolio',
+            name: 'portfolio',
+            component: require('./components/app_services_portfolio')
         }
     ],
     router = new VueRouter({routes});
+//=============================================================================
+/**
+ * components
+ */
+//=============================================================================
+const
+    AppNav = require('./components/app_nav'),
+    AppFooter = require('./components/app_footer');
 //=============================================================================
 /**
  * base VM
